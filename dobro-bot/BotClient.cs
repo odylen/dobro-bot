ea175fe467faff53e8efd8e063d0c539b3cd29cb
dylen <danilch176@gmail.com>
2020-03-09 00:20:14 +0300

    actions testing

--- dobro-bot/BotClient.cs
+++ dobro-bot/BotClient.cs
@@ -46,7 +46,7 @@ namespace dobro_bot
                 case "/keyboard":
                     ReplyKeyboardMarkup ReplyKeyboard = new[]
                     {
-                        new[] {"1.3", "1.6"},
+                        new[] {"1.3", "1.7"},
                     };
                     await Bot.SendTextMessageAsync(
                         chatId: message.Chat.Id,
